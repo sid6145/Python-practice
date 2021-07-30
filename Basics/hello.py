71404b5928cd56b62dba378eb1d405b004efbb2b
sid6145 <sidtdesh@gmail.com>
2021-07-30 14:56:49 +0530

    some python basics

---
+++ Basics/hello.py
@@ -0,0 +1,42 @@
+print("hello world")
+
+# this is a comment
+"""
+multiline comment
+which i can write here 
+here
+and here
+"""
+
+#variables
+x = 12
+y = 15
+z = "hello"
+print(z)
+
+#strings
+a = "hello mate "
+b = "Aye sir"
+print(a[0])
+
+#looping through strings
+for i in a:
+    print(i)
+
+#slicing string
+print(a[1:5])
+
+#string methods
+print(a.upper())
+print(a.lower())
+print(a + b)
+
+#list
+
+mylist = ["siddhant", "sam", "robert"]
+print(len(mylist))
+mylist[1] = "tony"
+
+mylist.append("stark")
+mylist.insert(2, "zebra")
+print(mylist)
